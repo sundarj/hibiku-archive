2b7f12e154835ebaf2765118c3454daa6bb8d7c7
radnus <sundarjoshi@hotmail.co.uk>
2015-07-01 16:06:14 +0000

    add custom merge for c9 branch

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const hibiku = require('hibiku');
 const compression = require('compression');
 
 hibiku.app.use(compression());
-hibiku.listen(process.env.PORT, process.env.IP);
+hibiku.listen(8080);
